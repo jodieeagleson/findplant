418a3bc3911960c0b9ee1d1fa3e9ea1a24b0e468
jodie-e <jodieeagleson@gmail.com>
2020-07-02 21:43:34 -0400

    search by common and scientific name

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,20 @@ import axios from 'axios';
 import path from 'path';
 console.log(`this is some text and i am writing it and you are reading it `);
 
+interface PlantFact {
+    scientific_name: string;
+    common_name: string;
+    id: number;
+}
+
+interface PlantImage {
+    url: string;
+}
+
+interface PlantFactComplete {
+    images: Array<PlantImage>;
+}
+
 const app = express();
 
 // app.get('/', function (req, res) {
@@ -12,17 +26,22 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('/plantfacts', (req, res) => {
+    console.log(req.query.plantname);
+    const userPlant = req.query.plantname;
     axios
-        .get('https://trefle.io/api/plants?page_size=5', {
+        .get(`https://trefle.io/api/plants?page_size=5&q=${userPlant}`, {
             headers: {
                 Authorization: 'Bearer aWFOWk16VWdEbWJ4WDQ1UGRBLy9iQT09',
             },
         })
-        .then((afact) => {
-            const listOfFacts = afact.data.map((fact: PlantFact) => {
-                return `this is a ${fact.common_name} known also as ${fact.scientific_name}`;
-            });
-            res.send(`<h1>Hello, plant friends!</h1> <br> <br> ${listOfFacts}`);
+        .then((axiosresponse) => {
+            // const listOfFacts = axiosresponse.data.map((fact: PlantFact) => {
+            //     if (fact.common_name === null) {
+            //         return `this is a  ${fact.scientific_name}. we don't know what it's called in plain english.`;
+            //     }
+            //     return `This is a  "${fact.scientific_name}" known also as "${fact.common_name}, which has an id of "${fact.id}".`;
+            // });
+            res.send(axiosresponse.data);
         })
         .catch((err) => {
             console.log(err);
@@ -36,11 +55,6 @@ app.get('/plantfacts', (req, res) => {
 });
 
 app.listen(3000);
-
-interface PlantFact {
-    scientific_name: string;
-    common_name: string;
-}
 
 // headers: {'X-Requested-With': 'XMLHttpRequest'},
 
